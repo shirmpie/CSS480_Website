1d9001b7accf54f474da26cb3671eaad910a95f6
shirmpie <91771141+shirmpie@users.noreply.github.com>
2024-04-03 15:03:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,20 @@ const getDragAfterElement = (
         }
     ).element;
 };
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
