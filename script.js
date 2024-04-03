203ad8f512c3be4b3bab1f0c3b867c73a1e2c032
shirmpie <91771141+shirmpie@users.noreply.github.com>
2024-04-03 13:50:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Function to enable list reordering
+function enableListReorder() {
+  const sortableList = document.getElementById('sortable-list');
+  sortableList.sortable = Sortable.create(sortableList, {
+    animation: 150,
+    onEnd: function(event) {
+      console.log("List reordered");
+    }
+  });
+}
 
+// Call the function to enable list reordering
+enableListReorder();
